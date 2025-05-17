ad678aae5d16607ea73001bc23739dc53b2bee64
Tyler Krasny <tekrasny@gmail.om>
2025-05-17 06:16:18 -0400

    listen on 0

--- src/main.ts
+++ src/main.ts
@@ -110,7 +110,7 @@ async function bootstrap() {
   });
 
   const port = configService.get('PORT', 3000);
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
